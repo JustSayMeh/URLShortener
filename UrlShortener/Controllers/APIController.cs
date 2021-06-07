636c2a9b1e873659b1e09600095443c72eaafbba
Олег Завьялов <O.romanowi4@gmail.com>
2021-06-07 20:55:19 +0700

    Поменял на POST и изменил регулярки

--- UrlShortener/Controllers/APIController.cs
+++ UrlShortener/Controllers/APIController.cs
@@ -11,7 +11,10 @@ using System.Threading.Tasks;
 
 namespace UrlShortener.Controllers
 {
-
+    public class URLRequest
+    {
+        public string url { get; set; }
+    }
     [Route("[controller]/[action]")]
     public class APIController : Controller
     {
@@ -20,23 +23,27 @@ namespace UrlShortener.Controllers
         private readonly string salt = "HkdfdjAler\\dsf";
         // base58 алфавит без символов в верхнем регистре
         private readonly string alphabet58 = "123456789abcdefghijkmnopqrstuvwxyz";
-        private Regex regex = new Regex(@"^((https:|http:)\/\/)?([а-яa-z0-9_-]+)(\.[а-яa-z0-9_-]+)+(\/[а-яa-z0-9_%-\.]*)*(\?[а-яa-z0-9_%-\.=#]*)?$");
+        private Regex regex = new Regex(@"^(?:http(s)?:\/\/)?([а-яА-ЯA-Za-z0-9_.-]+(?:\.[а-яА-ЯA-Za-z0-9_\.-]+))+[а-яА-ЯA-Za-z0-9_\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.]+$");
         private readonly SQLiteDbContext db = new SQLiteDbContext();
-        public JsonResult Create(string q) 
+        [HttpPost]
+        public JsonResult Create([FromBody] URLRequest q1) 
         {
+            string q = q1.url;
             // валидация переданного url
             Hashids hashids = new Hashids(salt, hash_size, alphabet58);
             Match m = regex.Match(q);
+            // получить хост
+            string domain = m.Groups[2].Value;
             if (!m.Success)
             {
                 this.HttpContext.Response.StatusCode = 400;
                 return Json(new Response("invalid link", ""));
             }
             // так как ссылки без http допустимы, то проверяем его наличие и добавляем в случае отсутствия
-            if (m.Groups[1].Length == 0)
+            if (!q.Contains("http"))
                 q = "https://" + q;
             // удаляем www для однородности ссылок
-            q = q.Replace("://www.", "://");
+            q = q.Replace(domain, domain.ToLower()).Replace("://www.", "://");
             // Получаем доменное имя с протоколом
             string domainName = Utils.GetRequestURLHead(HttpContext.Request);
             // получаем ссылку из бд
