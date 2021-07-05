3df6f247c8c9a2d0193140ea0a23cd865da37751
Олег Завьялов <O.romanowi4@gmail.com>
2021-07-06 00:50:04 +0700

    Uri вместо regex

--- UrlShortener/Controllers/APIController.cs
+++ UrlShortener/Controllers/APIController.cs
@@ -23,8 +23,6 @@ namespace UrlShortener.Controllers
         private readonly string salt = "HkdfdjAler\\dsf";
         // base58 алфавит без символов в верхнем регистре
         private readonly string alphabet58 = "123456789abcdefghijkmnopqrstuvwxyz";
-        private Regex regex = new Regex(@"^(?:http(s)?:\/\/)?([а-яА-ЯA-Za-z0-9_.-]+(?:\.[а-яА-ЯA-Za-z0-9_\.-]+))+[а-яА-ЯA-Za-z0-9_\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.%]+$");
-
         private readonly SQLiteDbContext db;
         public APIController(SQLiteDbContext db) => this.db = db;
         [HttpPost]
@@ -39,33 +37,35 @@ namespace UrlShortener.Controllers
             string q = q1.url;
             // валидация переданного url
             Hashids hashids = new Hashids(salt, hash_size, alphabet58);
-            Match m = regex.Match(q);
-            // получить хост
-            string domain = m.Groups[2].Value;
-            if (!m.Success)
+            if (!q.Contains("http"))
+                q = "https://" + q;
+            try
+            {
+                Uri uri = new Uri(q);
+                // получить хост
+                string domain = uri.Host;
+                // удаляем www для однородности ссылок
+                q = q.Replace(domain, domain.ToLower()).Replace("://www.", "://");
+                // Получаем доменное имя с протоколом
+                string domainName = Utils.GetRequestURLHead(HttpContext.Request);
+                // получаем ссылку из бд
+                Link flink = db.Links.FirstOrDefault(it => it.Original.Equals(q));
+                // Если ссылка не найдена, то создаем её и добавляем в бд
+                if (flink == null)
+                {
+                    string hash = hashids.EncodeLong(db.Links.Count() + 1);
+                    Link url = new Link(q, hash);
+                    db.Add(url);
+                    db.SaveChanges();
+                    return Json(new Response(domainName + "/" + prefix + hash, q));
+                }
+                return Json(new Response(domainName + "/" + prefix + flink.Short, flink.Original));
+            }
+            catch(UriFormatException e)
             {
                 this.HttpContext.Response.StatusCode = 400;
                 return Json(new Response("invalid link", ""));
             }
-            // так как ссылки без http допустимы, то проверяем его наличие и добавляем в случае отсутствия
-            if (!q.Contains("http"))
-                q = "https://" + q;
-            // удаляем www для однородности ссылок
-            q = q.Replace(domain, domain.ToLower()).Replace("://www.", "://");
-            // Получаем доменное имя с протоколом
-            string domainName = Utils.GetRequestURLHead(HttpContext.Request);
-            // получаем ссылку из бд
-            Link flink = db.Links.FirstOrDefault(it => it.Original.Equals(q));
-            // Если ссылка не найдена, то создаем её и добавляем в бд
-            if (flink == null)
-            {
-                string hash = hashids.EncodeLong(db.Links.Count() + 1);
-                Link url = new Link(q, hash);
-                db.Add(url);
-                db.SaveChanges();
-                return Json(new Response(domainName + "/" + prefix + hash, q));
-            }
-            return Json(new Response(domainName + "/" + prefix + flink.Short, flink.Original));
         }
     }
 }
